5814e1ae36a0d7080acc504984734be09b8aef93
EC2 Default User <ec2-user@ip-172-31-24-31.ap-northeast-1.compute.internal>
2024-07-26 13:40:48 +0000

    ユーザーごとの投稿取得成功0726

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,16 @@ class PostsController < ApplicationController
     
     def create
         @post = Post.new(post_params)
+        @post.user_id = current_user.id
         
-        if @post.save
+        if @post.save!
             flash[:notice] = "投稿しました。"
             Rails.logger.info "成功です。"
         
         else
+            render :new
             flash[:alert] = "投稿失敗しました。"
+            Rails.logger.info "失敗です。"
             
         end
     end
