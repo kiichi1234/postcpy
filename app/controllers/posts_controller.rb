e13efcdb740e43fda93e76cfd4408964abb865bd
EC2 Default User <ec2-user@ip-172-31-24-31.ap-northeast-1.compute.internal>
2024-08-31 09:55:46 +0000

    投稿検索一旦完成8/31

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,30 @@
 class PostsController < ApplicationController
    before_action :edit, only: %i(update)
    
-   def index
-        @category = params[:category]
-        @posts = Post.order(created_at: :desc)
-   end
+def index
+  @posts = Post.order(created_at: :desc) 
+  
+
+  if params[:search].present?
+    if params[:search][:category].present?
+      @category = params[:search][:category]
+      @posts = @posts.where(category: @category)
+    end
+
+    if params[:search][:word].present?
+      @word = params[:search][:word]
+      @posts = @posts.where("body LIKE ?", "%#{@word}%")
+    end
+    if params[:search][:category].present? && params[:search][:word].present?
+        @category = params[:search][:category]
+        @word = params[:search][:word]
+       @posts = Post.where("category = ? AND body LIKE ?", @category, "%#{@word}%")
+    end
+  end
+end
    
    def show
+       
         @post = Post.find(params[:id])
         @comment = Comment.new
         @comments = @post.comments.includes(:user).all
