dd5a1aad2842f95c9ee64593fc0b997b4e4d830e
EC2 Default User <ec2-user@ip-172-31-24-31.ap-northeast-1.compute.internal>
2024-09-20 09:54:17 +0000

    閲覧ユーザーraise処理とフラッシュメッセージ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,14 @@ class PostsController < ApplicationController
    before_action :edit, only: %i(update)
    
    
-def index
-  @posts = Post.order(created_at: :desc) 
-  @category = params.dig(:search, :category)
-  @word = params.dig(:search, :word)
-
-    @posts = @posts.where(category: @category) if @category.present?
-
-    @posts = @posts.where("body LIKE ?", "%#{@word}%") if @word.present?
-       
-
-end
+  def index
+      @posts = Post.order(created_at: :desc) 
+      @category = params.dig(:search, :category)
+      @word = params.dig(:search, :word)
+      @posts = @posts.where(category: @category) if @category.present?
+      @posts = @posts.where("body LIKE ?", "%#{@word}%") if @word.present?
+           
+  end
    
    def show
        
@@ -26,18 +23,25 @@ end
    end
     
     def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
-        
-        if !current_user.viewer? && @post.save
-            flash[:notice] = "投稿しました。"
-            redirect_to user_path(current_user)
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
+    
+      
+        if current_user.viewer? 
+            raise "閲覧ユーザーのため投稿出来ません。" 
+        elsif @post.save
+          respond_to do |format|
+            format.html { redirect_to user_path(current_user), notice: "投稿が成功しました。" }
+            format.turbo_stream { flash[:success] = "投稿成功しました！" }
+          end
         else
-           
-            flash[:alert] = "投稿失敗しました。"
-            render :new
+          raise "保存に失敗しました。"  
         end
+    rescue => e
+        flash[:alert] = e.message  
+        render :new  
     end
+    
     
     def edit
        @post = current_user.posts.find(params[:id])
